812196b3005bbedec33f02886650d214dbe21035
Matthieu Nantern <mnantern@xebia.fr>
2014-04-27 10:05:09 -0700

    Forgot AIX, like always...

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 describe 'ntp' do
 
 
-  ['Debian', 'RedHat','SuSE', 'FreeBSD', 'Archlinux', 'Gentoo'].each do |system|
+  ['AIX','Debian', 'RedHat','SuSE', 'FreeBSD', 'Archlinux', 'Gentoo'].each do |system|
     context "on #{system}" do
       let(:facts) {{ :osfamily => system }}
       it { should contain_class('ntp') }
