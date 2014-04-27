88d6fdfe25615e9bb0625785ad173b4b8a98a19c
Matthieu Nantern <mnantern@xebia.fr>
2014-04-27 10:15:15 -0700

    Adding tests to init.pp

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -6,6 +6,9 @@ describe 'ntp' do
     context "on #{system}" do
       let(:facts) {{ :osfamily => system }}
       it { should contain_class('ntp') }
+      it { should contain_class('ntp::install').that_comes_before('Class[ntp::config]') }
+      it { should contain_class('ntp::config').that_notifies('Class[ntp::service]') }
+      it { should contain_class('ntp::service') }
     end
   end
 end
